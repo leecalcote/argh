0f50b4beb77b40293563b3292338a0d1583be710
Gianluca Arbezzano <gianarb92@gmail.com>
2017-01-29 17:38:28 +0000

    update GitHub credential
    As fntlnz said it's clean approach work with GItHub HTTPS and not with
    SSL.

--- .jenkins/production-build.groovy
+++ .jenkins/production-build.groovy
@@ -16,17 +16,23 @@ node {
         git branch: "master",
             credentialsId: 'github-gianarb',
             poll: false,
-            url: "git@github.com:gianarb/argh.git"
+            url: "https://github.com/gianarb/argh"
     }
     stage("Build") {
-        sh 'echo $PATH'
-        sh 'make clean'
-        sh 'docker run --rm --name argh-${BUILD_NUMBER} -e GOPATH=/usr -e GOROOT=/usr/local/go -v /opt/jenkins/workspace/argh/:/usr/src/myapp -d -w /usr/src/myapp golang:1.7 sleep 500'
-        sh 'docker exec argh-${BUILD_NUMBER} go get ./...'
-        sh 'docker exec argh-${BUILD_NUMBER} go build -o ./build/argh .'
-        sh 'docker rm -fv argh-${BUILD_NUMBER}'
-        sh 'cat feeds.txt | ./build/argh generate ./docs'
-        sh 'git commit -a -m "rebuilding site `date`"'
-        sh 'git push origin master'
+        sh('make clean')
+        sh('docker run --rm --name argh-${BUILD_NUMBER} -e GOPATH=/usr -e GOROOT=/usr/local/go -v ${PWD}:/usr/src/myapp -d -w /usr/src/myapp golang:1.7 sleep 500')
+        sh('docker exec argh-${BUILD_NUMBER} go get ./...')
+        sh('docker exec argh-${BUILD_NUMBER} env GOOS=linux GOARCH=386 go build -o ./build/argh .')
+        sh('docker rm -fv argh-${BUILD_NUMBER}')
+        sh('cat feeds.txt | ./build/argh generate ./docs')
+        sh('git commit -a -m "rebuilding site `date`"')
+        withCredentials([[
+                        $class: 'UsernamePasswordMultiBinding',
+                        credentialsId: 'github-gianarb',
+                        usernameVariable: 'GIT_USERNAME',
+                        passwordVariable: 'GIT_PASSWORD'
+                ]]) {
+            sh('git push https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/gianarb/argh')
+        }
     }
 }
